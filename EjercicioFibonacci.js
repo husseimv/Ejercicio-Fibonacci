cc52437ecec2d0a933f90474088ff7e4e8fa867a
Husseim Vargas <142638555+husseimv@users.noreply.github.com>
2023-10-06 13:33:36 +0200

    Fibonacci

---
+++ EjercicioFibonacci.js
@@ -0,0 +1,14 @@
+const fib = (n, numFibInicio = [0, 1]) => {
+  if (n <= numFibInicio.length) {
+    return numFibInicio;
+  }
+
+  const nextValue =
+    numFibInicio[numFibInicio.length - 1] +
+    numFibInicio[numFibInicio.length - 2];
+  numFibInicio.push(nextValue);
+  return fib(n, numFibInicio);
+};
+
+const newFibonacci = fib(10);
+console.log(newFibonacci);
